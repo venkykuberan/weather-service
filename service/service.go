31a7a4a3b311d5578511d8c0e5afbe662441ab8f
venky kuberan <kuberan@google.com>
2021-05-23 19:51:55 -0700

    error handling modified in service handler

--- service/service.go
+++ service/service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"encoding/json"
-	"errors"
 	"log"
 	"net/http"
 	"os"
@@ -17,33 +16,37 @@ func WeatherHandler(w http.ResponseWriter, r *http.Request) {
 
 	if len(lat) == 0 || len(lon) == 0 {
 		w.WriteHeader(401)
-		w.Write([]byte("bad url"))
+		w.Write([]byte("Bad URL"))
 	} else {
 		weatherDto, err := provider.GetWeatherInfo(URL, os.Getenv("API_KEY"), lat, lon)
 		if err != nil {
-			log.Fatal(errors.New("Error fetching current weather information"))
+			log.Println("Error fetching current weather information :" + err.Error())
+			w.WriteHeader(500)
+			w.Write([]byte("Call to down stream API failed, please try again later"))
+		} else {
+
+			weatherInfo := CurrentWeather{}
+			// map the dto to service response
+			weatherInfo.Lat = weatherDto.Lat
+			weatherInfo.Lon = weatherDto.Lon
+			weatherInfo.TimeZone = weatherDto.Timezone
+			weatherInfo.Description = weatherDto.Description
+			weatherInfo.Temperature = weatherDto.Temp
+			weatherInfo.FeelsLike = weatherDto.FeelsLike
+			if len(weatherDto.Alerts) > 0 {
+				weatherInfo.Alerts = weatherDto.Alerts
+			}
+
+			bytes, err := json.Marshal(weatherInfo)
+
+			if err != nil {
+				log.Println("JSON serialization failed :" + err.Error())
+				w.WriteHeader(500)
+				w.Write([]byte("Internal Server Error"))
+			} else {
+				w.Write(bytes)
+			}
 		}
-
-		weatherInfo := CurrentWeather{}
-
-		// map the dto to service response
-		weatherInfo.Lat = weatherDto.Lat
-		weatherInfo.Lon = weatherDto.Lon
-		weatherInfo.TimeZone = weatherDto.Timezone
-		weatherInfo.Description = weatherDto.Description
-		weatherInfo.Temperature = weatherDto.Temp
-		weatherInfo.FeelsLike = weatherDto.FeelsLike
-		if len(weatherDto.Alerts) > 0 {
-			weatherInfo.Alerts = weatherDto.Alerts
-		}
-
-		bytes, err := json.Marshal(weatherInfo)
-
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-
-		w.Write(bytes)
 	}
 
 }
